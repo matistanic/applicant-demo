de3dac5f3aff55083ee446fd02cdf7c9d2a8b070
matistanic <matias.mullerg@mail.udp.cl>
2022-03-11 18:24:32 -0300

    agregado tooltip, popover y modal

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function(element){
+        return new bootstrap.Tooltip(element);
+    });
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    });
+    var myModal = new bootstrap.Modal(document.getElementById("myModal"), {
+        backdrop: "static"
+    });
+});
