9288848738b276327b445c0f9aa72a7306dd994e
matistanic <matias.mullerg@mail.udp.cl>
2022-03-11 22:21:29 -0300

    comentando codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(){
+    // tooltip
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     var tooltipList = tooltipTriggerList.map(function(element){
         return new bootstrap.Tooltip(element);
     });
+    // popover
         var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
         var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl)
     });
+    // modal estático
     var myModal = new bootstrap.Modal(document.getElementById("myModal"), {
         backdrop: "static"
     });
